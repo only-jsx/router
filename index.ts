3427c4df55b6cfb32bfb6414646612b67c30031b
Aleksey Vyatkin <avyatkin@gmail.com>
2022-11-24 00:20:10 +0300

    Formatted

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-export *  from './route';
-export {default as Route}  from './route';
+export * from './route';
+export { default as Route } from './route';
 export * from './router';
-export {default as Router} from './router';
+export { default as Router } from './router';
